880d3809f6c2ce4e4f9b9014655609ac07280e8f
cewthrowaway <cewthrowaway@gmail.com>
2024-11-20 18:05:06 +0000

    EEPROM started . Has some todos

--- .ccls-cache/@home@runner@SER486/Project 2@submission@eeprom.c
+++ .ccls-cache/@home@runner@SER486/Project 2@submission@eeprom.c
@@ -18,32 +18,12 @@ unsigned char write_busy;
 /* 7:0 */
 #define EEDR (*((volatile unsigned char *)0x40))
 #define EECR (*((volatile unsigned char *)0x3F))
-#define EEMPE 2
-#define EEPE 1
-#define EE_RDY (*((volatile unsigned char *)0x2C))
 
 /* Enabled when writebuf() places new data in the write buffer, this ISR sends
  * one byte at a time to the EEPROM. When the last byte is sent, it disables
  * futher EERPOM interrupts */
-/* should this bee vector 22 or 23?? Documents say 23 for EE ready */
 void __vector__22(){
-
-  /* if you haven't written all characters in then keep writing */
-  /* should we check to see if it is ready to write? */
-  if (bufidx < writesize) {
-    EEAR = writeaddr++;
-    EEDR = writebuf[bufidx++];
-  }
-  elseif(bufidx >= writesize) {
-    /* take EEPROM out of write mode */
-    EECR |= (0<<EEMPE);
-    EECR |= (0<<EEPE);
-
-    /* disable interrupts */
-    // TODO
-    /* make ready to write another */
-    write_busy = 0;
-  }
+  
 }
 
 /* this function places the data (specified buf and size) into the write buffer
@@ -54,12 +34,12 @@ void writebuf(unsigned int addr, unsigned char *buf, unsigned char size) {
   // check that write_busy is 0
   if (write_busy > 0) {
     // write to the console
-    //TODO
+
     return;
   }
   if (size > 64) {
     /* write error. buffer is too big */
-    // TODO
+
     return;
   }
   /* the EEPROM is now writing */
@@ -68,43 +48,19 @@ void writebuf(unsigned int addr, unsigned char *buf, unsigned char size) {
   writeaddr = addr;
   /* start reading from the beginning of the buffer */
   bufidx = 0;
+  /* copy the information to write */
+  /* copy buf to writebuf */
 
-  /* copy buf to writebuf and configure writesize */
-  //TODO
+  /* copy the size */
+  writesize = size;
 
-  /* set the EEPROM to write mode */
-  EECR |= (1<<EEMPE);
-  EECR |= (1<<EEPE);
-  
   /* enable the EEPROM ready interrupts */
-  //TODO
-  // set SREG and 
+  
 }
 
 /* this function reads a specified amount of data(size) from the EEPROM starting
  * at a specified address(addr) and places it in the specified buffer(buf) */
-void readbuf(unsigned int addr, unsigned char *buf, unsigned char size) {
-
-  // check that write_busy is 0
-  if (write_busy > 0) {
-    // write to the console
-
-    return;
-  }
-  if (size > 64) {
-    /* write error. buffer is too big */
-
-    return;
-  }
-  
-  // get the address
-  EEAR = writeaddr;
-  // set mode to read
-  EECR |= (1<<EERE);
-
-  // add character to buf 
-  
-}
+void readbuf(unsigned int addr, unsigned char *buf, unsigned char size) {}
 
 /* this functiosn returns 0 if write_busy is 0, otherwise, returns 1 */
-int isbusy() { return write_busy==1; }
+int isbusy() { return write_busy; }
---
+++ Project 2/submission/eeprom.h
@@ -0,0 +1,11 @@
+
+/* this function places the data (specified buf and size) into the write buffer
+ * for later writing to the EEPROM. The addr parameter specifies the location to
+ * write the data to. */
+/* This function should not be called when another write is in progress. */
+void writebuf(unsigned int, unsigned char *, unsigned char);
+/* this function reads a specified amount of data(size) from the EEPROM starting
+ * at a specified address(addr) and places it in the specified buffer(buf) */
+void readbuf(unsigned int, unsigned char *, unsigned char);
+/* this functiosn returns 0 if write_busy is 0, otherwise, returns 1 */
+int isbusy();
