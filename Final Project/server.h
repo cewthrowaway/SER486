514a185527ed38ce5c21e5086b1282a0c05d1e77
cewthrowaway <cewthrowaway@gmail.com>
2024-12-03 10:27:58 -0500

    Update server.h

--- Final Project/server.h
+++ Final Project/server.h
@@ -1,1 +1,33 @@
+#ifndef SERVER_H_INCLUDED
+#def SERVER_H_INCLUDED
 
+enum http_method {
+  GET, 
+  POST,
+  DELETE
+} http_method
+
+struct http_header {
+  char key[7],
+  char value[4]
+}
+struct http_request {
+  enum http_method method,
+  struct http_header headers[2],
+  char body[48]
+};
+
+struct http_response {
+  unsigned int status,
+  struct http_header headers[2],
+  char body[48]
+};
+  
+
+void server_init(char* socket);
+void server_start();
+void server_update();
+void server_send_error(char* message);
+void server_send_data(struct http_data);
+
+#endif
